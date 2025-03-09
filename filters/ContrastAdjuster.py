b49a9a483b9957c351ad05e30bb8b7f892aab699
DeptuchMateusz <135151523+DeptuchMateusz@users.noreply.github.com>
2025-03-09 21:04:34 +0100

    change of variable names

--- filters/ContrastAdjuster.py
+++ filters/ContrastAdjuster.py
@@ -5,7 +5,6 @@ def adjust_contrast(img_array, factor):
 
     mean_intensity = img_array.mean(axis=(0, 1), keepdims=True)
 
-    # Apply contrast adjustment (factor > 1 increases, factor < 1 decreases)
     contrast_image = np.clip((img_array - mean_intensity) * factor + mean_intensity, 0, 255).astype(np.uint8)
 
     return contrast_image
--- filters/Convolve.py
+++ filters/Convolve.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 def convolve(image, kernel):
-    kh, kw = kernel.shape
-    ih, iw = image.shape
-    pad_h, pad_w = kh // 2, kw // 2
+    kernel_height, kernel_width = kernel.shape
+    image_height, iw = image.shape
+    pad_h, pad_w = kernel_height // 2, kernel_width // 2
 
     padded_image = np.pad(image, ((pad_h, pad_h), (pad_w, pad_w)), mode='reflect')
     output = np.zeros_like(image)
 
-    for i in range(ih):
+    for i in range(image_height):
         for j in range(iw):
-            output[i, j] = np.sum(kernel * padded_image[i:i+kh, j:j+kw])
+            output[i, j] = np.sum(kernel * padded_image[i:i+kernel_height, j:j+kernel_width])
 
     return output
